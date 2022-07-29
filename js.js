5f0452f7f03897584967ef674e5960a2a6ae283d
Greg Schneider <greg53x@msn.com>
2022-07-28 22:51:14 -0600

    book catalogue object constructor

---
+++ js.js
@@ -0,0 +1,13 @@
+function Book(title, author, pages, read){
+    this.title = title;
+    this.author = author;
+    this.page = pages;
+    this.read = read;
+    this.info = function() {
+        let infoString = '';
+        infoString = title + ' by ' + author + ', ' + pages + ' pages, ' + read;
+        return infoString;
+    }
+}
+const locklands = new Book('Locklands','Robert Jackson Bennet',544,'read');
+console.log(locklands.info());
