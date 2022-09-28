599cf8b5eea907394e585a618ef2f7608963352d
Greg Schneider <greg53x@msn.com>
2022-09-27 22:44:20 -0600

    remove debugging lines and update comments

--- Library App/books.js
+++ Library App/books.js
@@ -6,8 +6,8 @@ const bookSubmitButton = document.querySelector('#bookSubmit');
 function Book(title, author, pages, read){
     this.title = title;
     this.author = author;
+    //check if supplied pages is a number or other
     let numTest = Number(pages);
-    console.log(numTest);
     ///relational comparison is always false when NaN is one of the operands
     if (Number.isNaN(numTest)) {
          this.pages = '';
@@ -21,7 +21,7 @@ function Book(title, author, pages, read){
         return infoString;
     }
 }
-//TODO - restrict page count to number only if(pages typeof ==num) etc
+//three sample books to have the library be preloaded with something
 const locklands = new Book('Locklands','Robert Jackson Bennet',544,true);
 const uzumaki = new Book('Uzumaki','Junji Ito',110,true);
 const cytonic = new Book('Cytonic','Brandon Sanderson',409,true);
@@ -29,6 +29,7 @@ myLibrary.push(locklands,uzumaki,cytonic);
 
 function displayLibraryBooks(books){
     books.forEach( book => {
+        //build table by creating a row, then inserting the relevant cell for each book
         let row = table.insertRow();
         let title = row.insertCell(0);
         title.textContent = book.title;
@@ -38,13 +39,12 @@ function displayLibraryBooks(books){
         pages.textContent = book.pages;
         
         let read = row.insertCell(3);
-        console.log(book.read)
         if (book.read) {
             read.innerHTML = '<input type=\"checkbox\" checked></input>'
         } else { read.innerHTML = '<input type=\"checkbox\" ></input>'}
         let trash = row.insertCell(4);
         
-        trash.innerHTML = `<button  >X</button>`;
+        trash.innerHTML = '<button>X</button>';
         
         // reference on cellIndex and rowIndex
         // https://stackoverflow.com/questions/45656949/how-to-return-the-row-and-column-index-of-a-table-cell-by-clicking
@@ -59,7 +59,6 @@ function displayLibraryBooks(books){
             let deleteIndex = x.closest('tr').rowIndex;
             if (confirm(`Are you sure you want to delete 
             ${table.rows[deleteIndex].cells[0].textContent }`)){
-                console.log(deleteIndex)
                 table.deleteRow(deleteIndex);
                 //remove book from library after deleting the table entry
                 myLibrary.splice(deleteIndex-1,1)
@@ -83,13 +82,17 @@ bookSubmitButton.addEventListener('click', () =>{
     if (userTitleInput.value == '') return;
     const userBook = new Book(userTitleInput.value,userAuthorInput.value,
         userPagesInput.value,userReadInput.checked);
-        console.log(typeof userBook.pages)
         //clears the user submission form
         userTitleInput.value = '';
         userAuthorInput.value = '';
         userPagesInput.value = '';
         userReadInput.value = '';
         myLibrary.push(userBook);
+        //delete the old library based on number of rows, 
+        //but with i=1 the header row stays - then rebuild from myLibrary with new entry.
+        //this likely doesn't scale well - consider modifying to append a new 
+        //row onto existing table.
+
         const tableRows = table.rows.length;
         for (let i = 1; i < tableRows;i++) {
             table.deleteRow(1);
